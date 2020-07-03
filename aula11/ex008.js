a06c640b4c824139be46fbaa283a947f1ec72a31
junior-camarinha <67797173+junior-camarinha@users.noreply.github.com>
2020-07-03 13:16:05 -0300

    Subir projetos

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // Condição Simples
+    console.log(`Voce ultrapassou A VELOCIDADE permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
